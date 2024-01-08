08a19d779d11137f9fe42121353854fe046f41d2
FuadBabayev <fuad.babayev19@mail.ru>
2024-01-08 09:07:13 +0400

    first version

---
+++ script.js
@@ -0,0 +1,492 @@
+let data = [
+    {
+        "firstName": "Jhon",
+        "lastName": "Doe",
+        "age": 40,
+        "mail": "jhon.doe@example.com",
+        "address": "Usa",
+        "phone": "555-785-99-68",
+        "birthDate": "01.01.2000"
+    },
+    {
+        "firstName": "Paula",
+        "lastName": "Hodkiewicz",
+        "age": 19,
+        "mail": "Paula_Hodkiewicz82@hotmail.com",
+        "address": "Monaside",
+        "phone": "281-707-95-41",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Jennifer",
+        "lastName": "Kessler",
+        "age": 24,
+        "mail": "Jennifer_Kessler60@hotmail.com",
+        "address": "East Sylvan",
+        "phone": "328-769-41-51",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Joseph",
+        "lastName": "Hammes",
+        "age": 55,
+        "mail": "Joseph_Hammes2@gmail.com",
+        "address": "Legroston",
+        "phone": "235-265-09-43",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Evelyn",
+        "lastName": "Pollich",
+        "age": 42,
+        "mail": "Evelyn26@gmail.com",
+        "address": "New Torey",
+        "phone": "511-050-35-79",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Evelyn",
+        "lastName": "Conn",
+        "age": 60,
+        "mail": "Evelyn70@gmail.com",
+        "address": "Abnerport",
+        "phone": "557-669-97-38",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Patrick",
+        "lastName": "Kovacek",
+        "age": 59,
+        "mail": "Patrick21@hotmail.com",
+        "address": "Fort Cathrynworth",
+        "phone": "047-857-46-04",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Willard",
+        "lastName": "Kirlin",
+        "age": 39,
+        "mail": "Willard.Kirlin75@yahoo.com",
+        "address": "Rogerschester",
+        "phone": "683-358-95-74",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Melissa",
+        "lastName": "Wisozk",
+        "age": 47,
+        "mail": "Melissa.Wisozk@yahoo.com",
+        "address": "Fort Victorfield",
+        "phone": "147-269-03-83",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Vera",
+        "lastName": "Wisoky",
+        "age": 34,
+        "mail": "Vera.Wisoky87@hotmail.com",
+        "address": "Mollyport",
+        "phone": "940-452-36-34",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Randall",
+        "lastName": "Bergstrom",
+        "age": 30,
+        "mail": "Bergstrom@hotmail.com",
+        "address": "East Andreschester",
+        "phone": "302-871-83-00",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Mildred",
+        "lastName": "Crooks",
+        "age": 43,
+        "mail": "Mildred65@hotmail.com",
+        "address": "Mission Viejo",
+        "phone": "962-176-43-97",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Margarita",
+        "lastName": "Beier",
+        "age": 18,
+        "mail": "Marrita_Beier14@gmail.com",
+        "address": "Stellaborough",
+        "phone": "349-239-25-52",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Deanna",
+        "lastName": "Roob",
+        "age": 21,
+        "mail": "Deanna16@yahoo.com",
+        "address": "South Allyfort",
+        "phone": "418-959-65-17",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Jennie",
+        "lastName": "Feil",
+        "age": 48,
+        "mail": "Jennie_Feil@gmail.com",
+        "address": "West Ardith",
+        "phone": "442-837-23-96",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Samuel",
+        "lastName": "Gerhold",
+        "age": 53,
+        "mail": "Samuel.Gerhold@hotmail.com",
+        "address": "Monroe",
+        "phone": "478-595-38-08",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Owen",
+        "lastName": "Pouros",
+        "age": 30,
+        "mail": "Owen_Pouros@hotmail.com",
+        "address": "Howellberg",
+        "phone": "794-282-47-22",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Jesse",
+        "lastName": "Johnston",
+        "age": 44,
+        "mail": "Jesse_Johnston29@gmail.com",
+        "address": "Sammiefort",
+        "phone": "515-455-67-95",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Gerardo",
+        "lastName": "Fisher",
+        "age": 31,
+        "mail": "Gerardo_Fisher4@gmail.com",
+        "address": "South Efrenberg",
+        "phone": "801-269-03-50",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Clay",
+        "lastName": "Hintz",
+        "age": 50,
+        "mail": "Clay_Hintz@gmail.com",
+        "address": "Pagacland",
+        "phone": "454-936-35-75",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Lawrence",
+        "lastName": "Blick",
+        "age": 51,
+        "mail": "Lawrence89@yahoo.com",
+        "address": "West Kirstenhaven",
+        "phone": "481-848-44-85",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Maxine",
+        "lastName": "Wilkinson",
+        "age": 24,
+        "mail": "Maxine38@hotmail.com",
+        "address": "Greenport",
+        "phone": "296-506-70-93",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Mathew",
+        "lastName": "Trantow",
+        "age": 51,
+        "mail": "Trantow84@hotmail.com",
+        "address": "Sarasota",
+        "phone": "782-173-07-24",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Tyrone",
+        "lastName": "Rippin",
+        "age": 56,
+        "mail": "Tyrone22@hotmail.com",
+        "address": "East Ninabury",
+        "phone": "079-798-13-22",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Clarence",
+        "lastName": "Romaguera",
+        "age": 59,
+        "mail": "Romaguera18@yahoo.com",
+        "address": "North Maeganberg",
+        "phone": "028-040-96-17",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Ronald",
+        "lastName": "Rosenbaum",
+        "age": 29,
+        "mail": "Ronald49@yahoo.com",
+        "address": "Tyler",
+        "phone": "049-306-16-02",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Rudolph",
+        "lastName": "Hagenes",
+        "age": 48,
+        "mail": "Rudolph.Hagenes@gmail.com",
+        "address": "West Chaddboro",
+        "phone": "424-439-75-89",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Doris",
+        "lastName": "Tromp",
+        "age": 42,
+        "mail": "Doris.Tromp28@yahoo.com",
+        "address": "South Mariane",
+        "phone": "804-345-45-34",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Owen",
+        "lastName": "Hessel",
+        "age": 59,
+        "mail": "Owen_Hessel@gmail.com",
+        "address": "Murray",
+        "phone": "164-096-59-59",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Mitchell",
+        "lastName": "Schultz",
+        "age": 43,
+        "mail": "Mitchell15@gmail.com",
+        "address": "Wileytown",
+        "phone": "455-859-31-47",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Misty",
+        "lastName": "Lind",
+        "age": 51,
+        "mail": "Misty_Lind47@yahoo.com",
+        "address": "Hoegercester",
+        "phone": "804-324-76-97",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Randal",
+        "lastName": "Crooks",
+        "age": 25,
+        "mail": "Randal.Crooks15@hotmail.com",
+        "address": "East Christopherland",
+        "phone": "659-812-07-49",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Colin",
+        "lastName": "Kihn-Bauch",
+        "age": 51,
+        "mail": "Colin.Kihn-Bauch64@gmail.com",
+        "address": "Port Ubaldo",
+        "phone": "662-920-88-41",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Jordan",
+        "lastName": "Sauer",
+        "age": 53,
+        "mail": "Jordan_Sauer@hotmail.com",
+        "address": "Littleborough",
+        "phone": "032-985-19-01",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Mabel",
+        "lastName": "Fahey",
+        "age": 27,
+        "mail": "Mabel_Fahey59@gmail.com",
+        "address": "Port Ulises",
+        "phone": "984-844-49-71",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Neil",
+        "lastName": "Wilkinson",
+        "age": 40,
+        "mail": "Neil12@gmail.com",
+        "address": "Erickaborough",
+        "phone": "168-110-52-11",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Donnie",
+        "lastName": "Breitenberg",
+        "age": 38,
+        "mail": "Don_Breit15@hotmail.com",
+        "address": "New Etha",
+        "phone": "420-530-24-62",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Spencer",
+        "lastName": "Tromp",
+        "age": 35,
+        "mail": "Spen_Tromp@gmail.com",
+        "address": "East Rudolph",
+        "phone": "412-934-12-33",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Tami",
+        "lastName": "Bernhard",
+        "age": 57,
+        "mail": "Tami.Bern65@yahoo.com",
+        "address": "Paxtonland",
+        "phone": "625-246-92-86",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "George",
+        "lastName": "Rippin",
+        "age": 32,
+        "mail": "GRippin84@yahoo.com",
+        "address": "Port Elisaborough",
+        "phone": "486-895-18-48",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Anne",
+        "lastName": "Lindgren",
+        "age": 26,
+        "mail": "Annen@hotmail.com",
+        "address": "Rennertown",
+        "phone": "426-517-92-49",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Lewis",
+        "lastName": "Kovacek",
+        "age": 51,
+        "mail": "Lewis53@gmail.com",
+        "address": "Francofield",
+        "phone": "318-786-49-87",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Myron",
+        "lastName": "Bednar",
+        "age": 21,
+        "mail": "Myron16@gmail.com",
+        "address": "Ziemeville",
+        "phone": "680-314-04-91",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Jana",
+        "lastName": "Nienow",
+        "age": 31,
+        "mail": "Jana.Nienow94@yahoo.com",
+        "address": "Port Wilber",
+        "phone": "579-744-98-56",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Sylvia",
+        "lastName": "Baumbach-Torphy",
+        "age": 31,
+        "mail": "Sylviah-Torphy@yahoo.com",
+        "address": "Bradleyboro",
+        "phone": "813-941-79-12",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Ivan",
+        "lastName": "Terry",
+        "age": 52,
+        "mail": "Ivan13@gmail.com",
+        "address": "Rolfsonstead",
+        "phone": "619-487-35-46",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Raymond",
+        "lastName": "Fritsch",
+        "age": 48,
+        "mail": "Fritsch3@hotmail.com",
+        "address": "Johnsworth",
+        "phone": "292-402-00-28",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Bertha",
+        "lastName": "Effertz",
+        "age": 30,
+        "mail": "Bertha_Effertz@gmail.com",
+        "address": "Huntington Park",
+        "phone": "609-100-15-61",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Kristopher",
+        "lastName": "Barrows",
+        "age": 51,
+        "mail": "Kristopher@gmail.com",
+        "address": "Cruickshankmouth",
+        "phone": "340-006-32-78",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Blake",
+        "lastName": "Feil",
+        "age": 58,
+        "mail": "Blake17@gmail.com",
+        "address": "Lednerville",
+        "phone": "051-503-22-75",
+        "birthDate": "05.01.2024"
+    },
+    {
+        "firstName": "Sabrina",
+        "lastName": "Berge",
+        "age": 27,
+        "mail": "Sabrina.Berge75@yahoo.com",
+        "address": "West Seneca",
+        "phone": "348-764-94-18",
+        "birthDate": "05.01.2024"
+    }
+];
+
+const root = document.querySelector("root");
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+const row = document.createElement("tr");
+root.appendChild(table);
+table.appendChild(thead);
+thead.appendChild(row);
+let object = data[0];
+let array = Object.entries(object);
+for (let i = 0; i < array.length; i++) {
+    const theader = document.createElement("th");
+    row.appendChild(theader);
+    row.className = "bg-dark text-capitalize";
+    theader.innerHTML += `${array[i][0]}`;
+    theader.className = "theadColumn";
+}
+table.appendChild(tbody);
+table.className = "table table-bordered table-hover table-striped table-light";
+for (const datas of data) {
+    let row = document.createElement("tr");
+    tbody.appendChild(row);
+    for (const key in datas) {
+        let column = document.createElement("td");
+        row.appendChild(column);
+        column.innerHTML = `${datas[key]}`;
+        column.className = 'column';
+    }
+}
+
